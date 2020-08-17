adec9d120b62f6eace505f14ad06acc8a2762f3e
Jani Muhlestein <janimuhlestein@gmail.com>
2020-08-17 12:13:20 -0600

    created submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-//get the new task button
-var buttonEl = document.querySelector("#save-task");
-//get the tasks to do id
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl=document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    debugger;
+    event.preventDefault();
+
     var listItemEL = document.createElement("li");
     listItemEL.className = "task-item";
     listItemEL.textContent = "This is a new task";
     tasksToDoEl.appendChild(listItemEL);
+    console.log(event);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler)
