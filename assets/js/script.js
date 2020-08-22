3d03bc524875a375e8484ff2f3c1a98e56851342
Jani Muhlestein <janimuhlestein@gmail.com>
2020-08-21 21:06:58 -0600

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -256,41 +256,15 @@ var saveTasks = function() {
 
 var loadTasks = function() {
     //get task items from local storage
-    tasks = localStorage.getItem("tasks");
+    var savedTasks = localStorage.getItem("tasks");
     console.log(tasks);
-    if(tasks === null) {
-    tasks = [];
+    if(!savedTasks) {
     return false;
     }
-    tasks = JSON.parse(tasks);
-    console.log(tasks);
-    for(var i = 0; i<tasks.length; i++) {
-        tasks[i].id = taskIdCounter;
-        var listItemEl = document.createElement("li");
-        listItemEl.setAttribute("data-task-id", tasks[i].id);
-        listItemEl.setAttribute("draggable", true);
-        listItemEl.className = "task-item";
-        var taskInfoEl = document.createElement("div");
-        taskInfoEl.className = "task-info";
-       // taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskDataObj.name+ "</h3><span class = 'task-type'>" + taskDataObj.type + "</span>";
-        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
-        listItemEl.appendChild(taskInfoEl);
-        var taskActionsEl = createTaskActions(tasks[i].id);
-        listItemEl.appendChild(taskActionsEl);
-        if(tasks[i].status === "to do") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
-            tasksToDoEl.appendChild(listItemEl);
-        }
-        else if(tasks[i].status --- "in progress") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1
-            tasksToDoEl.appendChild(listItemEl);
-        }
-        else {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2
-            tasksToDoEl.appendChild(listItemEl);
-        }
-        taskIdCounter ++;
-        console.log(listItemEl);
+    savedTasks = JSON.parse(savedTasks);
+    for(var i = 0; i < savedTasks.length; i++){
+        //pass each task object into the createTaskEl() function
+        createTaskEl(savedTasks[i]);
     }
 
 };
